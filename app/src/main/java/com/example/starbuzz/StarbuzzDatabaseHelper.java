ecb5c99d16b03b3be6bce1b97524f720625e9810
Dinesh-Desktop <dinesh.kumar@meritech.co.in>
2019-07-05 12:54:56 +0530

    Change insert function

--- app/src/main/java/com/example/starbuzz/StarbuzzDatabaseHelper.java
+++ app/src/main/java/com/example/starbuzz/StarbuzzDatabaseHelper.java
@@ -13,7 +13,7 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
 
     StarbuzzDatabaseHelper(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
-        Log.i("DineshTag","StarbuzzDatabaseHelper constructor");
+        Log.i("DineshTag", "StarbuzzDatabaseHelper constructor");
 
     }
 
@@ -28,32 +28,37 @@ public class StarbuzzDatabaseHelper extends SQLiteOpenHelper {
     }
 
     private static void insertDrink(SQLiteDatabase db, String name,
-                                   String description, int resourceId) {
+                                    String description, int resourceId) {
         ContentValues drinkValues = new ContentValues();
         drinkValues.put("NAME", name);
         drinkValues.put("DESCRIPTION", description);
         drinkValues.put("IMAGE_RESOURCE_ID", resourceId);
         db.insert("DRINK", null, drinkValues);
-        Log.i("DineshTag","insertDrink data inserted");
+
+        Log.i("DineshTag", "insertDrink data inserted");
     }
 
     private void updateMyDatabase(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (oldVersion < 1) {
-            Log.i("DineshTag","updateMyDatabase before create table");
+            Log.i("DineshTag", "updateMyDatabase before create table");
             db.execSQL("CREATE TABLE DRINK (_id INTEGER PRIMARY KEY AUTOINCREMENT, "
                     + "NAME TEXT,"
                     + " DESCRIPTION TEXT,"
                     + " IMAGE_RESOURCE_ID INTEGER);");
-
-            insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
-            insertDrink(db, "Cappuccino", "Espresso ,hot milk and steamed milk foam",
-                    R.drawable.cappuccino);
-            insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
-            Log.i("DineshTag","updateMyDatabase after create table");
+            Log.i("DineshTag", "updateMyDatabase after created");
+            insertInitData(db);
         }
         if (oldVersion < 2) {
             db.execSQL("ALTER TABLE DRINK ADD COLUMN FAVORITE NUMERIC;");
         }
+    }
+
+    private void insertInitData(SQLiteDatabase db) {
+        insertDrink(db, "Latte", "Espresso and steamed milk", R.drawable.latte);
+        insertDrink(db, "Cappuccino", "Espresso ,hot milk and steamed milk foam",
+                R.drawable.cappuccino);
+        insertDrink(db, "Filter", "Our best drip coffee", R.drawable.filter);
+        Log.i("DineshTag", "initial data inserted in table");
     }
 
 }
